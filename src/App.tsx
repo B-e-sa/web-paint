39cdd4ce1b97d81e755db3a126c055948ddc1bac
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-11-27 22:12:50 -0300

    add pencil width logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ import LeftBar from './components/leftBar/LeftBar';
 interface IPoint {
   x: number;
   y: number
-}
+};
 
 interface ILine {
   lastPosition: IPoint;
   position: IPoint
-}
+};
 
 interface IPencil {
   isActive: boolean;
   isMoving: boolean;
   position: IPoint;
   lastPosition: null | IPoint
-}
+};
 
-interface IRgba {
+export interface IRgba {
   r: number
   g: number
   b: number
   a: number
-}
+};
 
 const App = () => {
 
@@ -33,29 +33,26 @@ const App = () => {
     isMoving: false,
     position: { x: 0, y: 0 },
     lastPosition: null
-  }
+  };
 
-  const [pencilColor, setPencilColor] = useState<IRgba>()
-  const [pencilWidth, setPencilWidth] = useState<number>(1)
+  const [pencilColor, setPencilColor] = useState<IRgba>({ r: 0, g: 0, b: 0, a: 1 });
+  const [pencilWidth, setPencilWidth] = useState<number>(1);
 
   useEffect(() => {
 
-    const canvas: any = document.getElementById("canvas")
-    const ctx = canvas?.getContext("2d")
+    const canvas: any = document.getElementById("canvas");
+    const ctx = canvas?.getContext("2d");
 
     const drawLine = (line: ILine) => {
 
-      const { r, g, b, a } = pencilColor!
+      const { r, g, b, a } = pencilColor!;
 
-      ctx!.strokeStyle = `rgba(
-        ${r}, ${g}, ${b}, ${a}
-      )`
-
-      ctx.lineWidth = pencilWidth
-      ctx.beginPath()
-      ctx.moveTo(line.lastPosition.x, line.lastPosition.y)
-      ctx.lineTo(line.position.x, line.position.y)
-      ctx.stroke()
+      ctx!.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
+      ctx.lineWidth = pencilWidth;
+      ctx.beginPath();
+      ctx.moveTo(line.lastPosition.x, line.lastPosition.y);
+      ctx.lineTo(line.position.x, line.position.y);
+      ctx.stroke();
 
     }
 
@@ -65,26 +62,30 @@ const App = () => {
         drawLine({
           position: pencil.position,
           lastPosition: pencil.lastPosition
-        })
-      }
-      pencil.isMoving = false
+        });
+      };
+
+      pencil.isMoving = false;
 
       pencil.lastPosition = {
         x: pencil.position.x,
         y: pencil.position.y
-      }
+      };
 
-      setTimeout(handleDraw, 1)
+      setTimeout(handleDraw, 1);
 
-    }
+    };
 
-    handleDraw()
+    handleDraw();
 
-  }, [pencilColor])
+  }, [pencilColor, pencilWidth]);
 
   return (
     <div className="App">
-      <LeftBar props={[pencilColor, setPencilColor]} />
+      <LeftBar
+        pencilColor={[pencilColor, setPencilColor]}
+        pencilWidth={[pencilWidth, setPencilWidth]}
+      />
       <div id="canvas-container">
         <canvas
           id="canvas"
@@ -100,7 +101,7 @@ const App = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/leftBar/LeftBar.tsx
+++ src/components/leftBar/LeftBar.tsx
@@ -1,33 +1,48 @@
 import { ChromePicker } from 'react-color';
+import { IRgba } from '../../App';
 import './leftBar.sass';
 
-const LeftBar = ({ props }: any) => {
-    const defaultColors = [
-        "black",
-        "gray",
-        "white",
-        "blue",
-        "green",
-        "limegreen",
-        "yellow",
-        "red",
-        "orange",
-        "brown"
+const LeftBar = ({ pencilColor, pencilWidth }: any) => {
+
+    // the IRgb type will only pick
+    // rgba "objects", so no strings
+    // like "black" or "red"
+    const defaultColors: IRgba[] = [
+        { r: 0, g: 0, b: 0, a: 1 },       // black
+        { r: 206, g: 206, b: 206, a: 1 }, // white
+        { r: 255, g: 255, b: 255, a: 1 }, // blue
+        { r: 2, g: 245, b: 0, a: 1 },     // green
+        { r: 50, g: 205, b: 50, a: 1 },   // limegreen
+        { r: 255, g: 250, b: 46, a: 1 },  // yellow
+        { r: 255, g: 0, b: 0, a: 1 },     // red
+        { r: 255, g: 128, b: 0, a: 1 },   // orange
+        { r: 189, g: 103, b: 0, a: 1 },   // brown
+        { r: 249, g: 131, b: 255, a: 1 }  // pink
     ];
+
+    console.log(pencilWidth)
 
     return (
         <>
             <div id='leftbar-container'>
                 <ChromePicker
-                    color={props[0]}
-                    onChange={e => { props[1](e.rgb) }}
+                    color={pencilColor[0]}
+                    onChange={e => { pencilColor[1](e.rgb) }}
                 />
                 <div id='colors-grid'>
                     {defaultColors.map((item) => {
                         return (
                             <div
-                                onClick={() => props[1](item)}
-                                style={{ backgroundColor: item }}
+                                onClick={() => pencilColor[1](item)}
+                                style={{
+                                    backgroundColor:
+                                        `rgba(
+                                        ${item.r}, 
+                                        ${item.g}, 
+                                        ${item.b}, 
+                                        ${item.a}
+                                        )`
+                                }}
                                 className='color-chooser'
                             ></div>
                         )
@@ -35,9 +50,10 @@ const LeftBar = ({ props }: any) => {
                 </div>
                 <input
                     type="range"
-                    min={0}
+                    value={pencilWidth[0]}
+                    min={1}
                     max={100}
-                    onChange={e => console.log(e.currentTarget.value)}
+                    onChange={e => pencilWidth[1](e.currentTarget.value)}
                 />
             </div>
         </>
